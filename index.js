8c133ead628d0e972469a12f77d3614d0ba4e3fe
DJ Taylor <dtaylor88@gmail.com>
2021-08-30 23:38:03 -0700

    commit tree dff0672361fee9fdc36a60431bae936e691d2b5a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(arr, mapFunc) {
+    const newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        const result = mapFunc(arr[i], i, arr);
+        newArray.push(result);
+    }
+    return newArray;
+}
+
+function reduce(arr, reducer, initialValue) {
+    let total;
+    if (initialValue) {
+        total = initialValue;
+        for (let i = 0; i < arr.length; i++) {
+            total = reducer(arr[i], total);
+        }
+        return total;
+    } else {
+        total = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            total = reducer(arr[i], total);
+        }
+        return total;
+    }
+}
